d97644f1d4e097858936c8ac14b890178b525cdd
xingchao1350 <642410545@qq.com>
2020-02-16 22:19:10 +0800

    补充注释

--- csv_test.py
+++ csv_test.py
@@ -1,5 +1,5 @@
-import csv
-f=open('01.csv','r')
-table=csv.reader(f)
+import csv              #系统自带
+f=open('01.csv','r')    #以读取的目的打开文件
+table=csv.reader(f)     #读取文件并赋值给table列表
 for i in table:
-    print(i)
+    print(i)            #遍历元素
--- json_join_chn.py
+++ json_join_chn.py
@@ -1,12 +1,12 @@
-import json
-from os import listdir
-path = 'd:/python/code/t1/join'
-dirs = listdir(path)
+import json                             #系统自带
+from os import listdir                  #系统自带，获取当前目录
+path = 'd:/python/code/t1/join'         #指定目录
+dirs = listdir(path)                    #获取当前目录列表
 print(dirs)
-join_res=[]
+join_res=[]                             #定义空列表
 for i in range(len(dirs)):
-    with open('d:/python/code/t1/join/'+dirs[i], 'r', encoding='utf-8') as f:
-        join_temp = json.load(f)
-        join_res.extend(join_temp)
-with open('d:/python/code/t1/result/data1.json', 'w', encoding='utf-8') as f:
-    json.dump(join_res, f, ensure_ascii=False)
+    with open('d:/python/code/t1/join/'+dirs[i], 'r', encoding='utf-8') as f:   #循环读取当前目录下json文件
+        join_temp = json.load(f)                                                #赋值给临时列表
+        join_res.extend(join_temp)                                              #用expand方法追加列表
+with open('d:/python/code/t1/result/data1.json', 'w', encoding='utf-8') as f:   #以写入为目的打开文件
+    json.dump(join_res, f, ensure_ascii=False)                                  #写入文件，写入前会清空当前文件
